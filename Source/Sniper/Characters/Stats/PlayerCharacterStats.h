9cb25123c15101e3d9bf8bad7dc46fa217858ddb
TaiPrev <taiprev@gmail.com>
2021-03-02 10:40:28 +0100

    Retouches 1

--- Source/Sniper/Characters/Stats/PlayerCharacterStats.h
+++ Source/Sniper/Characters/Stats/PlayerCharacterStats.h
@@ -1,15 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameCharacterStats.h"
-//#include "../../Utilities/Types.h"
 #include "PlayerCharacterStats.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class SNIPER_API UPlayerCharacterStats : public UGameCharacterStats
 {
@@ -20,32 +13,7 @@ class SNIPER_API UPlayerCharacterStats : public UGameCharacterStats
 
 	protected:
 		UPROPERTY(EditDefaultsOnly, Category = "Player", meta = (DisplayName = "Lives"))
-			int m_iLifes;
-		UPROPERTY(EditDefaultsOnly, Category = "Player", meta = (DisplayName = "Max Lives"))
-			int m_iMaxLifes;
-		UPROPERTY(EditDefaultsOnly, Category = "Player", meta = (DisplayName = "Bullets"))
-			int m_iBullets;
-		UPROPERTY(EditDefaultsOnly, Category = "Player", meta = (DisplayName = "Max Bullets"))
-			int m_iMaxBullets;
-
-	public:
-		UFUNCTION(BlueprintCallable)
-			void SetLifes(int _iValue);
-		UFUNCTION(BlueprintCallable)
-			int GetLifes();
-		UFUNCTION(BlueprintCallable)
-			void SubtractLife();
-		UFUNCTION(BlueprintCallable)
-			void AddLife();
-
-		UFUNCTION(BlueprintCallable)
-			void SetMaxLifes(int _iValue);
-		UFUNCTION(BlueprintCallable)
-			int GetMaxLifes();
-
-		UFUNCTION(BlueprintCallable)
-			void SetBullets(int _iValue);
-		UFUNCTION(BlueprintCallable)
-			int GetBullets();
-
+			FIntWLimits m_Lives;
+		UPROPERTY(EditDefaultsOnly, Category = "Player", meta = (DisplayName = "Lives"))
+			FIntWLimits m_Bullets;
 };
